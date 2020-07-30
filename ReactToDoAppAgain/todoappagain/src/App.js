69fe57f1e8e32807c41d6ddc97cabcc88ba346ff
Azusa Okamoto <curiousazunas@gmail.com>
2020-07-30 18:11:40 +0900

    タスクの編集機能を作成

--- ReactToDoAppAgain/todoappagain/src/App.js
+++ ReactToDoAppAgain/todoappagain/src/App.js
@@ -42,12 +42,31 @@ class App extends Component {
     event.target.title.value='';
   }
 
-  deleteItem(id) {
+  /* 
+  filterメソッドで、クリックされた要素がもつidと異なるidをもつ要素のみを
+  絞り込み、それらで新しく配列を作成する。そしてその新しい配列をsetStateする
+  ことで、反映される。
+  spliceやpop等のように、インデックス番号を指定する必要はないため、
+  クリックイベントで使うことが可能。
+  */
+  deleteTodo(id) {
     let filteredTodos = this.state.todos.filter(todo =>
       todo.id !== id);
       this.setState({
         todos: filteredTodos
       })
+  }
+
+  handleEdit(title, id) {
+    let todos = this.state.todos;
+    todos.map(todo => {      
+      if (todo.id === id){
+        todo.title = title;
+      }
+    })
+    this.setState({
+      todos: todos
+    })
   }
 
   render() {
@@ -56,7 +75,8 @@ class App extends Component {
         <Form handleSubmit={this.handleSubmit.bind(this)} />
         <ToDoList
         todos={this.state.todos}
-        deleteItem={this.deleteItem.bind(this)}
+        handleEdit={this.handleEdit.bind(this)}
+        deleteTodo={this.deleteTodo.bind(this)}
         />
         </div>
     );
