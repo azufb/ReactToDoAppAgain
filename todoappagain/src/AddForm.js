66d9953651d6d154ec5bd7e08ac978224bfa2aa4
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-24 20:47:16 +0900

    追加フォームの作成

--- todoappagain/src/AddForm.js
+++ todoappagain/src/AddForm.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
 
-function AddForm() {
- return (
-     <div>
-         <form>
-            <input type='text' required />
-            <input type='submit' value='ADD' />
-         </form>
-     </div>
- )   
+class AddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+
+    render() {
+        return (
+            <form>
+                <input type='text'  required/>
+                <input type='submit' value='ADD' />
+            </form>
+        )
+    }
 }
 
 export default AddForm;
