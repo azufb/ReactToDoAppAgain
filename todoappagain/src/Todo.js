eb59d677d79f06243f109f3e63db4fc87db22285
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-25 16:00:08 +0900

    削除機能を付けた

--- todoappagain/src/Todo.js
+++ todoappagain/src/Todo.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deleteTodo } from './Store';
 
 class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: []
+        }
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete(event) {
+        event.preventDefault();
+        let action = deleteTodo(this.state.title);
+        this.props.dispatch(action);
+        this.setState({
+            title: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 { this.props.value.title }
+                <input type='button' value='delete'
+                onClick={ this.handleDelete } />
             </div>
         )
     }
 }
 
-export default connect()(Todo);
+export default connect((state) => state)(Todo);
